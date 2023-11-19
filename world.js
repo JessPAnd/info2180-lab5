f9beef5067de6dab72c8c1478082d02434e6b6b0
Jessica Anderson <jespaiand@hotmail.com>
2023-11-19 06:19:57 -0500

    Edited Starter Files, Exercise 5

--- world.js
+++ world.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     const lookupButton = document.getElementById('lookup');
+    const lookupButton2 = document.getElementById('lookup2');
     const resultDiv = document.getElementById('result');
 
     lookupButton.addEventListener('click', function() {
@@ -21,5 +22,25 @@ document.addEventListener('DOMContentLoaded', function() {
                 console.log(error);
                 
             })       
+    });
+
+    lookupButton2.addEventListener('click', function() {
+        let country = document.getElementById('country').value;
+        fetch(`http://localhost:8080/info2180-lab5/world.php?country=${country}&lookup=cities`)
+        .then((response)=>{
+            if (response.ok){
+                return response.text();
+            }
+            else
+            {
+                return Promise.reject(Error);
+            }
+        })
+        .then((data) =>{
+            resultDiv.innerHTML = data;
+        })
+        .catch((error)=>{
+            console.log(error);
+        })       
     });
 });
--- world.php
+++ world.php
@@ -9,36 +9,69 @@ $dbname = 'world';
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
 
 $country = isset($_GET['country']) ? trim($_GET['country']) : '';
+$lookup = isset($_GET['lookup']) ? $_GET['lookup'] : '';
 
-if ($country !== '') {
-    $stmt = $conn->prepare("SELECT * FROM countries WHERE name LIKE :country");
-    $country = "%$country%";
-    $stmt->bindParam(':country', $country);
-} else {
-    $stmt = $conn->prepare("SELECT * FROM countries");
-}
+if ($lookup === 'cities') {
+    if ($country !== '') {
+        $stmt = $conn->prepare("SELECT cities.name, cities.district, cities.population FROM cities JOIN countries ON cities.country_code = countries.code WHERE countries.name LIKE :country");
+        $country = "%$country%";
+        $stmt->bindParam(':country', $country);
+    } else {
+        $stmt = $conn->prepare("SELECT cities.name, cities.district, cities.population FROM cities JOIN countries ON cities.country_code = countries.code");
+    }
 
-$stmt->execute();
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    $stmt->execute();
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-echo "<table>";
-echo "<thead>";
-echo "<tr>";
-echo "<th>Name</th>";
-echo "<th>Continent</th>";
-echo "<th>Independence Year</th>";
-echo "<th>Head of State</th>";
-echo "</tr>";
-echo "</thead>";
-echo "<tbody>";
-foreach ($results as $row) {
+    echo "<table>";
+    echo "<thead>";
     echo "<tr>";
-    echo "<td>" . $row['name'] . "</td>";
-    echo "<td>" . $row['continent'] . "</td>";
-    echo "<td>" . $row['independence_year'] . "</td>";
-    echo "<td>" . $row['head_of_state'] . "</td>";
+    echo "<th>Name</th>";
+    echo "<th>District</th>";
+    echo "<th>Population</th>";
     echo "</tr>";
+    echo "</thead>";
+    echo "<tbody>";
+    foreach ($results as $row) {
+        echo "<tr>";
+        echo "<td>" . $row['name'] . "</td>";
+        echo "<td>" . $row['district'] . "</td>";
+        echo "<td>" . $row['population'] . "</td>";
+        echo "</tr>";
+    }
+    echo "</tbody>";
+    echo "</table>";
+} else {
+    if ($country !== '') {
+        $stmt = $conn->prepare("SELECT * FROM countries WHERE name LIKE :country");
+        $country = "%$country%";
+        $stmt->bindParam(':country', $country);
+    } else {
+        $stmt = $conn->prepare("SELECT * FROM countries");
+    }
+
+    $stmt->execute();
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    echo "<table>";
+    echo "<thead>";
+    echo "<tr>";
+    echo "<th>Name</th>";
+    echo "<th>Continent</th>";
+    echo "<th>Independence Year</th>";
+    echo "<th>Head of State</th>";
+    echo "</tr>";
+    echo "</thead>";
+    echo "<tbody>";
+    foreach ($results as $row) {
+        echo "<tr>";
+        echo "<td>" . $row['name'] . "</td>";
+        echo "<td>" . $row['continent'] . "</td>";
+        echo "<td>" . $row['independence_year'] . "</td>";
+        echo "<td>" . $row['head_of_state'] . "</td>";
+        echo "</tr>";
+    }
+    echo "</tbody>";
+    echo "</table>";
 }
-echo "</tbody>";
-echo "</table>";
 ?>
